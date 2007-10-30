9100ffb17ac9df6f89f81c8c53f5235432b701b7
root <root@puppetmaster.black.co.at>
2007-10-30 19:46:31 +0100

    dnsmasq: moved to module, added interface variable, fix template call, enhance notifications

---
+++ manifests/init.pp
@@ -0,0 +1,36 @@
+# dnsmasq.pp - configure a small dns resolver and dhcp server
+# Copyright (C) 2007 David Schmitt <david@schmitt.edv-bus.at>
+# See LICENSE for the full license granted to you.
+
+
+class dnsmasq {
+	package { dnsmasq:
+		ensure => installed,
+		notify => Service[dnsmasq],
+	}
+	# TODO: configure per-firewall settings
+	file { 
+		"/etc/dnsmasq.d":
+			ensure => directory, checksum => mtime,
+			mode => 0755, owner => root, group => root,
+			notify => Service[dnsmasq];
+		"/etc/dnsmasq.d/puppet":
+			content => template("dnsmasq/dnsmasq.conf"),
+			mode => 0644, owner => root, group => root,
+			notify => Service[dnsmasq];
+	}
+	line {
+		dir_include:
+			file => "/etc/dnsmasq.conf",
+			line => "conf-dir=/etc/dnsmasq.d",
+			require => File["/etc/dnsmasq.conf"],
+			notify => Service[dnsmasq],
+	}
+
+	file { "/etc/dnsmasq.conf": checksum => md5, require => Package["dnsmasq"] }
+	service { dnsmasq:
+		ensure => running,
+		pattern => "/usr/sbin/dnsmasq",
+		subscribe => [ File["/etc/dnsmasq.conf"], File["/etc/hosts"] ]
+	}
+}
